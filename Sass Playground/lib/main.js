19515edbc51e1a7d64ac953833701bf0c2416703
Sw33tT00th <jsl245@nau.edu>
2016-01-23 10:11:05 -0700

    functioning carousel is now in place

--- Sass Playground/lib/main.js
+++ Sass Playground/lib/main.js
@@ -1,1 +1,20 @@
 //Main Javascript File
+$(document).ready(function() {
+	var next = 0;
+	carousel(next);
+});
+
+function carousel(next) {
+	var displayImage = $('#display-image');
+	var allImages = $('#carousel-images').children();
+
+	setTimeout(function() {
+		$('#display-image').fadeTo(500, 0);
+		setTimeout(function() {
+			displayImage.attr('src', allImages[next].src);
+		}, 500);
+		$('#display-image').fadeTo(500, 1);
+		next = (next + 1) % allImages.length;
+		carousel(next);
+	}, 5000);
+}
